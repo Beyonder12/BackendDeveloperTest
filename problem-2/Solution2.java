796046a9aa84a4ca3c675873a57b96343df86e53
Fajr <fajri.illahi@medicaboo.com>
2021-06-12 10:12:23 +0700

    The description of problem 2

---
+++ problem-2/Solution2.java
@@ -0,0 +1,29 @@
+/*
+Create a method to find the lowest cost from edge to center
+move vertically and horizonatally
+
+case #1
+
+7 3 4
+8 6 9
+1 2 5
+
+#Answer 1
+2 + 6 = 8 thats the minimum.
+
+
+case #2
+
+9 3 5 4 8
+5 1 4 2 7
+6 5 7 9 1
+
+#Answer 2
+???
+
+ */
+
+
+public class Solution2 {
+
+}
